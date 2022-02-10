7ff2da71f38ab6af53e4cfb3e6b9e300b2a1e905
laura-bluz <laura.bluz@hotmail.com>
2022-02-10 17:49:05 -0300

    Terminando Menu mobile

--- menu.js
+++ menu.js
@@ -1,12 +1,18 @@
 const btnMobile = document.getElementById('btn-mobile');
+const btnMobileClose = document.getElementById('btn-mobile-close');
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+btnMobileClose.addEventListener('click', toggleMenu);
+btnMobileClose.addEventListener('click', toggleMenu);
 
 function toggleMenu(event) {
     if (event.type === 'touchstart')
         event.preventDefault();
     const nav = document.getElementById('topo-nav');
     nav.classList.toggle('active');
+    if (event.type === 'click' || 'touchstart')
+        btnMobileClose.classList.toggle('no-active');
 }
 
